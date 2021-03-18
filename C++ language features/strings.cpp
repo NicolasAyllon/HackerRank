f230e58ffa98b46c3c71a99b9701e3212ffda0b8
NicolasAyllon <51245273+NicolasAyllon@users.noreply.github.com>
2021-03-17 20:33:52 -0500

    updated comments

--- C++ language features/strings.cpp
+++ C++ language features/strings.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main() {
     
-	// Declare string and read input
+	// Declare strings and read input
     string a;
     string b;
     cin >> a >> b;
     
-    // Create strings a_2 to match a but with b's first character
+    // Create string a_2 to match a but with b's first character
     // Create string b_2 to match b but with a's first character
     string a_2 = a;
     string b_2 = b;
     a_2[0] = b[0]; // change a_2's first character to b's
     b_2[0] = a[0]; // change b_2's first character to a's
     
+    // print lengths, concatenation, and a_2 b_2 on separate lines
     cout << a.size() << " " << b.size() << endl;
     cout << a + b << endl;
     cout << a_2 << " " << b_2 << endl;
