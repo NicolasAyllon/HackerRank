76434a587d2518c597c68a0a38024c1fd0f51dda
NicolasAyllon <51245273+NicolasAyllon@users.noreply.github.com>
2021-03-17 20:31:40 -0500

    added strings

---
+++ C++ language features/strings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+	// Declare string and read input
+    string a;
+    string b;
+    cin >> a >> b;
+    
+    // Create strings a_2 to match a but with b's first character
+    // Create string b_2 to match b but with a's first character
+    string a_2 = a;
+    string b_2 = b;
+    a_2[0] = b[0]; // change a_2's first character to b's
+    b_2[0] = a[0]; // change b_2's first character to a's
+    
+    cout << a.size() << " " << b.size() << endl;
+    cout << a + b << endl;
+    cout << a_2 << " " << b_2 << endl;
+    
+    return 0;
+}
