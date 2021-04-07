c7b960765b499ea26855a545cd055c3d149237f6
NicolasAyllon <51245273+NicolasAyllon@users.noreply.github.com>
2021-04-06 21:08:20 -0500

    added truncated code from main()

--- C++ language features/attending workshops.cpp
+++ C++ language features/attending workshops.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 //Define the structs Workshops and Available_Workshops.
@@ -73,4 +72,25 @@ int CalculateMaxWorkshops(Available_Workshops* available_workshops) {
     }
     return maximizedSchedule.size();
 }
+
+
+// From HackerRank
 int main(int argc, char *argv[]) {
+    int n; // number of workshops
+    cin >> n;
+    // create arrays of unknown size n
+    int* start_time = new int[n];
+    int* duration = new int[n];
+
+    for(int i=0; i < n; i++){
+        cin >> start_time[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> duration[i];
+    }
+
+    Available_Workshops * ptr;
+    ptr = initialize(start_time,duration, n);
+    cout << CalculateMaxWorkshops(ptr) << endl;
+    return 0;
+}
