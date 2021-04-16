15a437b65f1b8a2ff491d216081623e72ba8893c
NicolasAyllon <51245273+NicolasAyllon@users.noreply.github.com>
2021-04-16 18:31:20 -0500

    added insert node at tail of linked list

---
+++ Interview preparation challenges/insert node at tail of linked list.cpp
@@ -0,0 +1,17 @@
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    // Empty list: set head to point at new node
+    if(head == nullptr) {
+        head = new SinglyLinkedListNode(data);
+        return head;
+    }
+    // Program has reached this point, so list is not empty
+    // Create current pointer and traverse list until pointing to last node
+	SinglyLinkedListNode* current = head;
+    while(current->next != nullptr) {
+        current = current->next;
+    }
+    // Now current points to the last node in the list
+    // Point current->next to new node, which is now at the end of the list
+    current->next = new SinglyLinkedListNode(data);
+    return head;
+}
