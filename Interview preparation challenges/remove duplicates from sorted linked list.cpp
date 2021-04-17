8181c4cc509055f5671edf1b665166322ae303c8
NicolasAyllon <51245273+NicolasAyllon@users.noreply.github.com>
2021-04-17 15:40:38 -0500

    shortened and improved remove duplicates from sorted linked list

--- Interview preparation challenges/remove duplicates from sorted linked list.cpp
+++ Interview preparation challenges/remove duplicates from sorted linked list.cpp
@@ -1,31 +1,25 @@
 // Solution:
 // Remove duplicates from linked list
 SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head) {
-    //If the list is empty return it
-    if(head == nullptr) return head;
-    // List is not empty
-    SinglyLinkedListNode* unique_head = head; // point to head of unique list, returned
-    SinglyLinkedListNode* unique_tail = head; // points to tail of unique list
+    //If the list is empty or has just 1 node, return it
+    if(head == nullptr || head->next == nullptr) return head;
+    // List has more than 1 node
     SinglyLinkedListNode* current = head; // current traverses list starting at head
-    SinglyLinkedListNode* temp = nullptr; // will point to nodes to be deleted
-    // advance current to next node
-    current = current->next;
-    while(current != nullptr) {
-        // If current node matches the tail of the unique list, delete it and advance
-        if(current->data == unique_tail->data) {
-            temp = current;
-            current = current->next;
-            delete temp;
+    SinglyLinkedListNode* nodeToDelete = nullptr; // temporarily store nodes to delete
+    //
+    while(current->next != nullptr) {
+        // If next node's data matches this one's
+        if(current->data == current->next->data) {
+            // mark it for deletion
+            nodeToDelete = current->next;
+            // set current's next to point to the one after the next node
+            current->next = current->next->next;
+            delete nodeToDelete;
         }
-        // Current differs from unique_tail, so append to unique_tail
-        // and advance both current and unique_tail;
+        // Next node's data is different, so advance current
         else {
-            unique_tail->next = current;
-            unique_tail = current;
             current = current->next;
         }
     }
-    // After reaching end of list, set tail's next to null
-    unique_tail->next = nullptr;
-    return unique_head;
+    return head;
 }
